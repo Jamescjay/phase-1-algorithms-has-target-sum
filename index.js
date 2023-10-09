4e08f5e51eabdc10b15f7ac1f830a8e5cae72150
James <james.ekasiba@student.moringaschool.com>
2023-10-08 23:01:38 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const Numbers =  new Set();
+  
+  for (const num of array) {
+    const difference = target - num;
+    if (Numbers.has(difference)) {
+      return true;
+    }
+    Numbers.add(num);
+  }
+  return false;
 }
 
+
+
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+   Create a set to store numbers in the array
+   Iterate through the array
+   Calculate the difference between the target and the current number
+   If the difference is in the set, we've found a pair that adds up to the target, return true
+   Otherwise, add the current number to the set
+   If we finish iterating through the array without finding a pair, return false
 */
 
 /*
-  Add written explanation of your solution here
+  The hasTargetSum function uses a set to keep track of the numbers encountered while iterating through the array. If the difference between the target and the current number is found in the set, it means there is a pair that adds up to the target, and the function returns true. If no such pair is found in the set, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([9, 4, 18, 2, 14, 5], 30));
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
